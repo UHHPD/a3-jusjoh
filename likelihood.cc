136205ae61353462c84a2fa026b9bc510a0d332e
jwilckens <102976232+jwilckens@users.noreply.github.com>
2022-05-09 15:36:50 +0000

    likelihood

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+using namespace std;
 
 
-double poisson(double mu, int k) {
-    return 0;
+double poisson(double mu, int k){//Berechnet die Poissonwahrscheinlichkeit (mu^k)*exp(-mu)/k!
+  double muk = pow(mu,k);
+  double expmu = exp(-mu);
+  double k_fak = tgamma(k+1);
+  return muk*expmu/k_fak;
+}
+
+double prob(vector<int> daten, double mu){
+
+  
 }
 
 int main() {
-    using namespace std;
-
-
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  ifstream fin("datensumme.txt");
+  int k_i, zahl;
+  vector<int> daten;
+  for(int i = 0 ; i < 234 ; ++i) {
+    fin >> zahl;
+    daten.push_back(zahl);
+  }
+  fin.close();
 }
